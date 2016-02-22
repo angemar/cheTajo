0992ab27a2484551bcd8ae3e00698f8a960fc88c
Starnino <francescostarna@hotmail.com>
2016-02-22 19:59:53 +0100

    App initialized

---
+++ Application/app/src/main/java/com/teamlz/notifier/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.teamlz.notifier;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIMER = 3000;
+    private ProgressBar progbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //add immersive mode
+        getWindow().getDecorView().setSystemUiVisibility(
+                          View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        //finish immersive mode
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+                startActivity(intent);
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_TIMER);
+    }
+
+}
